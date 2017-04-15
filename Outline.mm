1db4ea9fc5f25115db7d7d734765752de4615db1
Rakiteer1 <tabulebdeh@gmail.com>
2017-04-15 10:05:41 +0300

    Added Freemind Outline
    This will be like a check list, to help me get the work done.

---
+++ Outline.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1492239464727" ID="ID_1078434858" MODIFIED="1492239751359" TEXT="&#x62a;&#x62f;&#x631;&#x64a;&#x633; &#x627;&#x644;&#x642;&#x627;&#x639;&#x62f;&#x629; &#x627;&#x644;&#x646;&#x648;&#x631;&#x627;&#x646;&#x64a;&#x629; &#x644;&#x644;&#x62d;&#x644;&#x642;&#x627;&#x62a;">
+<node CREATED="1492239763524" ID="ID_1117762231" MODIFIED="1492239774302" POSITION="left" TEXT="&#x635;&#x641;&#x62d;&#x629; &#x627;&#x644;&#x639;&#x646;&#x648;&#x627;&#x646;"/>
+<node CREATED="1492239774643" ID="ID_1170891461" MODIFIED="1492239780671" POSITION="left" TEXT="&#x627;&#x62e;&#x62a;&#x628;&#x627;&#x631;&#x627;&#x644;&#x645;&#x633;&#x62a;&#x648;&#x649;"/>
+<node CREATED="1492239781891" ID="ID_396955972" MODIFIED="1492239786159" POSITION="left" TEXT="&#x627;&#x644;&#x645;&#x642;&#x62f;&#x645;&#x629;"/>
+<node CREATED="1492239788515" ID="ID_1809356095" MODIFIED="1492239793217" POSITION="left" TEXT="&#x627;&#x644;&#x639;&#x631;&#x636;"/>
+</node>
+</map>
