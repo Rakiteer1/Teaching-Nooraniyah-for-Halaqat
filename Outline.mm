e1cc84de9fe4da9f84a4efd71bd524570f637151
Rakiteer1 <tabulebdeh@gmail.com>
2017-04-17 15:10:09 +0300

    Added Outline to the Document
    Adjusted the outline slightly.
    Added the outline to the document.

--- Outline.mm
+++ Outline.mm
@@ -1,9 +1,24 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1492239464727" ID="ID_1078434858" MODIFIED="1492239751359" TEXT="&#x62a;&#x62f;&#x631;&#x64a;&#x633; &#x627;&#x644;&#x642;&#x627;&#x639;&#x62f;&#x629; &#x627;&#x644;&#x646;&#x648;&#x631;&#x627;&#x646;&#x64a;&#x629; &#x644;&#x644;&#x62d;&#x644;&#x642;&#x627;&#x62a;">
-<node CREATED="1492239763524" ID="ID_1117762231" MODIFIED="1492239774302" POSITION="left" TEXT="&#x635;&#x641;&#x62d;&#x629; &#x627;&#x644;&#x639;&#x646;&#x648;&#x627;&#x646;"/>
-<node CREATED="1492239774643" ID="ID_1170891461" MODIFIED="1492239780671" POSITION="left" TEXT="&#x627;&#x62e;&#x62a;&#x628;&#x627;&#x631;&#x627;&#x644;&#x645;&#x633;&#x62a;&#x648;&#x649;"/>
-<node CREATED="1492239781891" ID="ID_396955972" MODIFIED="1492239786159" POSITION="left" TEXT="&#x627;&#x644;&#x645;&#x642;&#x62f;&#x645;&#x629;"/>
-<node CREATED="1492239788515" ID="ID_1809356095" MODIFIED="1492239793217" POSITION="left" TEXT="&#x627;&#x644;&#x639;&#x631;&#x636;"/>
+<node CREATED="1492239464727" ID="ID_1078434858" MODIFIED="1492422015308" TEXT="&#x62a;&#x62f;&#x631;&#x64a;&#x633; &#x627;&#x644;&#x642;&#x627;&#x639;&#x62f;&#x629; &#x627;&#x644;&#x646;&#x648;&#x631;&#x627;&#x646;&#x64a;&#x629; &#x644;&#x644;&#x62d;&#x644;&#x642;&#x627;&#x62a;">
+<font NAME="Noto Naskh Arabic" SIZE="12"/>
+<node CREATED="1492239763524" ID="ID_1117762231" MODIFIED="1492422015307" POSITION="left" TEXT="&#x635;&#x641;&#x62d;&#x629; &#x627;&#x644;&#x639;&#x646;&#x648;&#x627;&#x646;">
+<font NAME="Noto Naskh Arabic" SIZE="12"/>
+</node>
+<node CREATED="1492239774643" ID="ID_1170891461" MODIFIED="1492422015304" POSITION="left" TEXT="&#x627;&#x62e;&#x62a;&#x628;&#x627;&#x631;&#x627;&#x644;&#x645;&#x633;&#x62a;&#x648;&#x649;">
+<font NAME="Noto Naskh Arabic" SIZE="12"/>
+</node>
+<node CREATED="1492239781891" ID="ID_396955972" MODIFIED="1492422015303" POSITION="left" TEXT="&#x627;&#x644;&#x645;&#x642;&#x62f;&#x645;&#x629;">
+<font NAME="Noto Naskh Arabic" SIZE="12"/>
+</node>
+<node CREATED="1492239788515" ID="ID_1809356095" MODIFIED="1492422015303" POSITION="left" TEXT="&#x627;&#x644;&#x639;&#x631;&#x636;">
+<font NAME="Noto Naskh Arabic" SIZE="12"/>
+</node>
+<node CREATED="1492421934900" ID="ID_376050691" MODIFIED="1492422015300" POSITION="left" TEXT="&#x62f;&#x644;&#x64a;&#x644; &#x645;&#x62e;&#x627;&#x631;&#x62c; &#x627;&#x644;&#x62d;&#x631;&#x648;&#x641; &#x648;&#x627;&#x644;&#x635;&#x641;&#x627;&#x62a;">
+<font NAME="Noto Naskh Arabic" SIZE="12"/>
+</node>
+<node CREATED="1492421957252" ID="ID_1006615929" MODIFIED="1492422015291" POSITION="left" TEXT="&#x641;&#x643;&#x631;&#x629; &#x623;&#x633;&#x626;&#x644;&#x629; &#x627;&#x645;&#x644;&#x626; &#x627;&#x644;&#x641;&#x631;&#x627;&#x63a; &#x628;&#x62c;&#x62f;&#x627;&#x648;&#x644; &#x62a;&#x647;&#x62c;&#x64a; &#x627;&#x644;&#x643;&#x644;&#x645;&#x627;&#x62a;">
+<font NAME="Noto Naskh Arabic" SIZE="12"/>
+</node>
 </node>
 </map>
